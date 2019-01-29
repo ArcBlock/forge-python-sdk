c80588a4653c1ff40ef55c9d7f881ef586cb0dc0
Riley Shu <riley@arcblock.io>
2019-01-29 07:49:19 -0800

    Add license and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forge-python-sdk",
+    version="0.0.2",
+    author="Riley Shu",
+    author_email="riley@arcblock.io",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArcBlock/forge-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'grpcio',
+        'grpcio-tools',
+        'toml',
+    ],
+)
