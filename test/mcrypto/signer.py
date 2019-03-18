b94a3ed444d520cafdfbfdda7f94340c7199d1f8
Riley Shu <riley@arcblock.io>
2019-03-17 23:45:13 -0700

    signer test

---
+++ test/mcrypto/signer.py
@@ -0,0 +1,24 @@
+import unittest
+
+from forge.mcrypto import signer
+
+
+class SignerTest(unittest.TestCase):
+
+    def test_ed25519_sk_to_pk(self):
+        pass
+
+    def test_ed25519_sign(self):
+        pass
+
+    def test_ed25519_verify(self):
+        pass
+
+    def test_secp256k1_sk_to_pk(self):
+        pass
+
+    def test_secp256k1_sign(self):
+        [ass]
+
+    def test_secp256k1_verify(self):
+        pass
